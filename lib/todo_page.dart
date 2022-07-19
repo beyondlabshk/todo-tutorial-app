8d516754399f1f7f8e1d1669aff2608744dba61f
Samuel Chan <cchin.samuel@gmail.com>
2022-07-19 16:44:06 +0700

    feat: initial commit

---
+++ lib/todo_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class Todo {
+  bool isComplete;
+  String name;
+  Todo({
+    this.name = '',
+    this.isComplete = false,
+  });
+}
+
+class TodoPage extends StatefulWidget {
+  const TodoPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  List<Todo> todos = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My To do App'),
+      ),
+      body: ListView(
+        children: _buildTodos(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            todos.add(Todo());
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  _buildTodos() {
+    List<Widget> todoWidgets = [];
+    todos.asMap().forEach(
+      (index, element) {
+        todoWidgets.add(
+          ListTile(
+            title: TextFormField(
+              initialValue: element.name,
+              onChanged: (val) {
+                todos[index].name = val;
+              },
+            ),
+            leading: Checkbox(
+              value: element.isComplete,
+              onChanged: (val) {
+                setState(() {
+                  todos[index].isComplete = !element.isComplete;
+                });
+              },
+            ),
+          ),
+        );
+      },
+    );
+    return todoWidgets;
+  }
+}
